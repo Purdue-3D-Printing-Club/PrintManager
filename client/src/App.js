933bf33de828a7013a88d256663c5f2c3b8d8117
thomp907 <andrewtho5942@gmail.com>
2024-03-20 20:37:31 -0400

    Add update and delete functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,65 @@
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import Axios from 'axios'
 
 function App() {
+  
+  const [printerName, setPrinterName] = useState('');
+  const [printerBrand, setPrinterBrand] = useState('');
+  const [printerList, setPrinterList] = useState([]);
+  const [updatePrinterVal, setUpdatePrinter] = useState('');
+
+  useEffect(()=> {
+    Axios.get('http://localhost:3001/api/get').then((response) => {
+      setPrinterList(response.data);
+    });
+  }, []);
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3001/api/insert', {
+      printerName: printerName,
+       printerBrand: printerBrand
+      });
+      setPrinterList([...printerList, {printerName: printerName, brand: printerBrand}]);
+  };
+
+  const deletePrinter = (name) => {
+    Axios.delete(`http://localhost:3001/api/delete/${name}`);
+  }
+
+  const updatePrinter = (name) => {
+    Axios.put('http://localhost:3001/api/update', {
+      printerName: name,
+      printerBrand: updatePrinterVal
+    });
+    setUpdatePrinter("");
+  };
+
   return (
     <div className="App">
       <h1>--Print Manager--</h1>
       <div className="form">
         <label>Printer Name:</label>
-        <input type="text" name="printerName"/>
+        <input type="text" name="printerName" onChange={(e) => {
+          setPrinterName(e.target.value);
+        }}/>
 
         <label>Brand:</label>
-        <input type="text" name="printerBrand"/>
+        <input type="text" name="printerBrand" onChange={(e) => {
+          setPrinterBrand(e.target.value);
+        }}/>
 
-        <button>Submit</button>
+        <button onClick={submitReview}>Submit</button>
+        {printerList.map(val => {
+          return <div className="printerCard" key={val.printerName}>
+              <h1>{val.printerName}</h1> 
+                <p>Printer Brand: {val.brand}</p>
+                <button onClick={() => {deletePrinter(val.printerName)}}>Delete</button>
+                <input id="updateInput" type="text" onChange={(e) => {setUpdatePrinter(e.target.value)}}/>
+                <button onClick={() => {updatePrinter(val.printerName)}}>Update</button>
+              </div>
+        })}
       </div>
-      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
@@ -8,18 +10,53 @@ const db = mysql.createConnection({
     password: "password",
     database: "printmanagerdb",
 });
-/*db.connect((err) => {
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+db.connect((err) => {
     if (err) {
       console.error('Error connecting to MySQL:', err);
       return;
     }
     console.log('Connected to MySQL');
-  });*/
+  });
 
-app.get('/', (req, res) => {
-    const sqlInsert = "INSERT INTO printer (printerName, brand) VALUES ('Joseppa', 'Prusa');"
-    db.query(sqlInsert, (err, result) => {
-        res.send("hello andrew test 2");
+app.get('/api/get', (req, res) =>{
+    const sqlSelect = "SELECT * FROM printer";
+    db.query(sqlSelect, (err, result) => {
+        res.send(result);
+    });
+});
+
+app.post('/api/insert', (req, res) => {
+
+    const printerName = req.body.printerName;
+    const printerBrand = req.body.printerBrand;
+
+    const sqlInsert = "INSERT INTO printer (printerName, brand) VALUES (?,?)";
+    db.query(sqlInsert, [printerName, printerBrand], (err, result) => {
+        console.log(result);
+    });
+});
+
+app.delete('/api/delete/:printerName', (req, res) => {
+    const name = req.params.printerName;
+    const sqlDelete = "DELETE FROM printer WHERE printerName=?";
+
+    db.query(sqlDelete, name, (err, res) => {
+        if (err) console.log(err);
+    });
+});
+
+app.put('/api/update', (req, res) => {
+    const name = req.body.printerName;
+    const brand = req.body.printerBrand;
+    const sqlUpdate = "UPDATE printer SET brand = ? WHERE printerName = ?";
+
+    db.query(sqlUpdate, [brand, name], (err, res) => {
+        if (err) console.log(err);
     });
 });
 
