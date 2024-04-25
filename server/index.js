f8e6eeaaab523b849c1e33c2a6218a3e085ed913
thomp907 <andrewtho5942@gmail.com>
2024-04-25 14:34:28 -0400

    Add pie charts to summary page

--- server/index.js
+++ server/index.js
@@ -6,13 +6,14 @@ const cors = require('cors');
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "password",
-    database: process.env.DB_NAME || "printmanagerdb",
+    host: "printmanager-server.database.windows.net",// "localhost",
+    user: "andrewtho5942", //|| "root",
+    password:  "E'xpX@L>6?!u;cE",//|| "password",
+    database: "printmanagerdb", //|| "printmanagerdb",
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    ssl: true
 });
 
 app.use(cors());
